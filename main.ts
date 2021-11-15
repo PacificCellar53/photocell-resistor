3bb911a7b64800dee972afdf27a904ee816daabf
PacificCellar53 <92798379+PacificCellar53@users.noreply.github.com>
2021-11-15 09:06:43 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+let PhotoCell_Sensor = 0
+basic.forever(function () {
+    PhotoCell_Sensor = pins.analogReadPin(AnalogPin.P0)
+    basic.showNumber(PhotoCell_Sensor)
+    if (0 < 0) {
+    	
+    } else {
+    	
+    }
+})
